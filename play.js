53e7f34b94721d374ef3fbdf992e8d311fbb2606
Simon <simonolsen388@gmail.com>
2019-10-23 19:56:07 +0000

    set up client side of snake game

---
+++ play.js
@@ -0,0 +1,25 @@
+// 192.168.88.149
+
+const net = require("net");
+const stdin = process.stdin;
+
+//Establishes connection with the game server
+
+const connect = () => {
+  const conn = net.createConnection({
+    host: "192.168.88.149",
+    port: 50541
+  });
+
+  //interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  stdin.on("data", data => {
+    conn.write(data);
+  });
+
+  return conn;
+};
+
+console.log("Connecting...");
+connect();
